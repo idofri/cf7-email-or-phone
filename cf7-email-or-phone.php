751e69b07f372d679157720db76ea2e214200b20
Ido Friedlander <ido@imark.co.il>
2019-12-15 21:02:34 +0200

    fixed indentation

--- cf7-email-or-phone.php
+++ cf7-email-or-phone.php
@@ -9,47 +9,47 @@
 
 class Cf7EmailOrPhone
 {
-	private $tags = [];
+    private $tags = [];
 
     public static function instance() {
-		static $instance;
-		return $instance ?? ($instance = new static);
-	}
+        static $instance;
+        return $instance ?? ($instance = new static);
+    }
 
-	public function __construct()
-	{
-		// Remove default validations
-		remove_filter('wpcf7_validate_tel*', 'wpcf7_text_validation_filter');
-		remove_filter('wpcf7_validate_email*', 'wpcf7_text_validation_filter');
+    public function __construct()
+    {
+        // Remove default validations
+        remove_filter('wpcf7_validate_tel*', 'wpcf7_text_validation_filter');
+        remove_filter('wpcf7_validate_email*', 'wpcf7_text_validation_filter');
 
-		// Apply custom validations
-		add_filter('wpcf7_validate_tel*', [$this, 'skipValidation'], 10, 2);
-		add_filter('wpcf7_validate_email*', [$this, 'skipValidation'], 10, 2);
+        // Apply custom validations
+        add_filter('wpcf7_validate_tel*', [$this, 'skipValidation'], 10, 2);
+        add_filter('wpcf7_validate_email*', [$this, 'skipValidation'], 10, 2);
 
-		// Finalize logic
-		add_filter('wpcf7_validate', [$this, 'afterValidations']);
-	}
+        // Finalize logic
+        add_filter('wpcf7_validate', [$this, 'afterValidations']);
+    }
 
-	public function afterValidations($result)
-	{
-		if (count($this->tags) >= 2) {
-			foreach ($this->tags as $tag) {
-				$result->invalidate($tag, wpcf7_get_message('invalid_required'));
-			}
-		}
-		return $result;
-	}
+    public function afterValidations($result)
+    {
+        if (count($this->tags) >= 2) {
+            foreach ($this->tags as $tag) {
+                $result->invalidate($tag, wpcf7_get_message('invalid_required'));
+            }
+        }
+        return $result;
+    }
 
-	public function skipValidation($result, $tag)
-	{
-		$name = $tag->name;
-		$value = isset($_POST[$name]) ? trim(wp_unslash(strtr((string) $_POST[$name], "\n", " "))) : '';
-		if (empty($value)) {
-			$this->tags[] = $tag;
-			return $result;
-		}
-		return wpcf7_text_validation_filter($result, $tag);
-	}
+    public function skipValidation($result, $tag)
+    {
+        $name = $tag->name;
+        $value = isset($_POST[$name]) ? trim(wp_unslash(strtr((string) $_POST[$name], "\n", " "))) : '';
+        if (empty($value)) {
+            $this->tags[] = $tag;
+            return $result;
+        }
+        return wpcf7_text_validation_filter($result, $tag);
+    }
 }
 
 add_action('wpcf7_init', function() {
